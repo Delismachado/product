6ada16776f5f1a55d21c5b9862e71b831ee5bb33
Derli Machado <dam.machado1@gmail.com>
2020-12-08 11:25:55 -0300

    Add: create and list categories.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 class Product:
     __id: int
     __name: str
-    __description: str    
+    __description: str
+    __categories: list[str]
 
-    def __init__(self, name: str, description: str) -> None:
+
+    def __init__(self, name: str, description: str, categories: []) -> None:
         self.set_id()
         self.set_name(name)
         self.set_description(description)
+        self.set_categories = []
         
 
     def set_id(self) -> None:
-        if len(products) == 0:
-            self.__id = 1
-        else:
-            self.__id = products[-1].get_id() + 1
+        self.__id = id        
 
     def get_id(self) -> int:
         return self.__id
@@ -30,12 +30,18 @@ class Product:
     def get_description(self) -> str:
         return self.__description
 
+    def set_categories(self, categories: list[str]) -> None:
+        self.__categories = categories
+
+    def get_categories(self) -> list[str]:
+        return self.__categories
+
 
 class Category:
     __id: int
     __name: str
 
-    def __init__(self, name: str, ) -> None:
+    def __init__(self, name: str) -> None:
         self.set_id()
         self.set_name(name)
 
@@ -52,31 +58,57 @@ class Category:
         return self.__name       
 
 products = []
+categories = []
+
+
+def create_category():
+    print("Create a new category:")
+    name = input("Category name:")
+
+    category = Category(name)
+    categories.append(category)
+
+    menu()
 
 def create_product():
     print("Create a new product")
     name = input("Product name: ")
-    description = input("Product description: ")    
+    description = input("Product description: ")
+    category = input("Product category: ")   
 
-    product = Product(name, description)
+    product = Product(name, description, category)
     products.append(product)
     print("Cool, registered product!")
 
     menu()
 
+
+
+def list_categories():
+    if len(categories) == 0:
+        print("No categories available!")
+    else:
+        print("Categories:")
+        for category in categories:
+            print("Name: {category.get_name()}")
+    menu()
+    
+
+
 def find_product(id: int) -> int:
-    index = -1
+    idx = -1
     for product in products:
         if product.get_id() == id:
-            index = products.index(product)
+            idx = products.index(product)
             break
     
-    return index
+    return idx
 
-def print_product(index: int) -> None:
-        print(f"ID: {products[index].get_id()}")
-        print(f"Name: {products[index].get_name()}")
-        print(f"Description: {products[index].get_description()}")
+def show_product(idx: int) -> None:
+        print(f"Id: {products[idx].get_id()}")
+        print(f"Name: {products[idx].get_name()}")
+        print(f"Description: {products[idx].get_description()}")
+        print(f"Category: {products[idx].get_category()}")
         
 
 def list_products():
@@ -95,17 +127,17 @@ def update_product():
     print("Update a product")
     id = int(input("Product id: "))
 
-    index = find_product(id)
+    idx = find_product(id)
 
-    if index >= 0:
+    if idx >= 0:
         print("Product found!")
-        print_product(index)
+        show_product(idx)
 
         name = input("New product name: ")
         description = input("New product description: ")        
 
-        products[index].set_name(name)
-        products[index].set_description(description)        
+        products[idx].set_name(name)
+        products[idx].set_description(description)        
 
         print("Product updated!")
     else:
@@ -117,16 +149,16 @@ def delete_product():
     print("Delete a product")
     id = int(input("Product id: "))
 
-    index = find_product(id)
+    idx = find_product(id)
 
-    if index >= 0:
+    if idx >= 0:
         print("Product found!")
-        print_product(index)
+        show_product(idx)
         
         opt = input("You want to delete? (Y/N) ")
         
         if opt == "Y":
-            products.pop(index)
+            del products[idx]
             print("Product deleted!")
     else:
         print("Product not found!")
@@ -141,7 +173,10 @@ def menu():
         print("2 - List")
         print("3 - Update")
         print("4 - Delete")
+        print("5 - List category")
+        print("6 - Create category")
         print("0 - Exit")
+        
         opt = int(input("opt: "))
 
         if opt == 0:
@@ -154,6 +189,10 @@ def menu():
             update_product()            
         elif opt == 4:
             delete_product()
+        elif opt == 5:
+            list_categories()
+        elif opt == 6:
+            create_category()
         else:
             print("Ivalid opt!")  
                 
