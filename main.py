d02da73aecad66b5ddcdbc7abbf8168a50282c7d
Derli Machado <dam.machado1@gmail.com>
2020-12-22 16:07:42 -0300

    fix: metodos.

--- main.py
+++ main.py
@@ -4,53 +4,163 @@ from category import Category
 products = []
 categories = []
 
-def create_product():
-  pass
-def list_products():
-  pass
-def update_product():
-  pass
-def delete_product():
-  pass
-def find_product():
-  pass
-def create_category():
-  pass
+def valid_number(number: float) -> float:
+    try:
+        while float(number) <= 0:
+            number = float(input('try something greater than 0 '))
+    except ValueError:
+        number = valid_number(input('Need to be a number: '))
+    return number
+
+def new_category():
+    print('\nCreate category')
+    
+    category = Category()
+    
+    category.set_name(input("Category name: "))
+    if not categories:
+        category.set_id(1)
+        categories.append(category)
+    else:
+        new_id = categories[-1].get_id() + 1
+        category.set_id(new_id)
+        categories.append(category)
+    print("successfully!")
+
 def list_categories():
-  pass
+    print('\nCategories: ')
+    
+    for c in categories:
+        print(c)
+    
+def new_product() -> None:
+    print('Create product')
+    
+    product = Product()
+    sku_not_valid = True
+    sku = ""
+    while sku_not_valid:
+        sku = input('Product SKU: ')
+        p = search_product(sku)
+        if not p:
+            sku_not_valid = False
+            break
+        print('SKU invalid, try again')
+    
+    product.set_sku(sku)
+    name = input('Product name: ')
+    product.set_name(name)
+    price = input('Product price: ')
+    product.set_price(valid_number(price))
+    weight = input('Product weight: ')
+    product.set_weight(valid_number(weight))
+    height = input('Product height: ')
+    product.set_height(valid_number(height))
+    length = input('Product length: ')
+    product.set_length(valid_number(length))
+    width = input('Product width: ')
+    product.set_width(valid_number(width))
+    description = input('Product description: ')
+    product.set_description(description)
+
+    products.append(product)
+
+
+def list_products():
+    if len(products) > 0:
+        print('\nList products')
+        for product in products:
+            print(product)
+    else:
+        print('\nNo products available.')
+
+
+def input_sku():
+    initial_product = input('Product SKU: ')
+    return search_product(initial_product)
+
+
+def search_product(sku: str) -> Product:
+    for product in products:
+        if product.get_sku() == sku:
+            return product
+
+
+def product_detail():
+    p = input_sku()
+    if not p:
+        print('Product not found.')
+    else:
+        print(p)
+
+
+def delete_product():
+    p = input_sku()
+    if not p:
+        print('Product not found.')
+    else:
+        products.remove(p)
+        print('Product deleted successfully')
+
+
+def edit_product():
+    p = input_sku()
+    if not p:
+        print('Product not found.')
+    else:
+        print(p)
+        
+        name = input('Product name: ')
+        p.set_name(name)
+        price = input('Product price: ')
+        p.set_price(valid_number(price))
+        weight = input('Product weight: ')
+        p.set_weight(valid_number(weight))
+        height = input('Product height: ')
+        p.set_height(valid_number(height))
+        length = input('Product length: ')
+        p.set_length(valid_number(length))
+        width = input('Product width: ')
+        p.set_width(valid_number(width))
+        description = input('Product description: ')
+        p.set_description(description)
+
 
 def menu():
-    options = ["Create product", "List product", "Edit product",
-          "Delete product", "Find by id", "Create category",
-          "List category", "Exit"]
-    print("\nMENU")
+    options = ['Create product', 'Edit Product', 'List Product', 
+               'Find Product by SKU', 'Delete Product', 'Create Category', 
+               'List Categories', 'Exit']
+
+    print('\nMENU: ')
 
     for i, option in enumerate(options):
-      print(f"[{i+1}] - {option}")
+        print(f'[{i+1}] - {option}')
 
-    op = int(input("\nSelect option:"))
+    op = int(input('Select one option: '))
     return op
 
-    while True:
-      try:
+
+while True:
+    try:
         op = menu()
         if op == 1:
-            create_product()
+            new_product()
         elif op == 2:
-            list_products()
+            edit_product()
         elif op == 3:
-            update_product()  
+            list_products()
         elif op == 4:
-            delete_product()  
+            product_detail()
         elif op == 5:
-            create_category()
+            delete_product()
         elif op == 6:
-            list_categories()
+            new_category()
         elif op == 7:
-          exit(0) 
+            list_categories()
+        elif op == 0:
+            exit(0)
         else:
-            print("Invalid opt!")
-      except ValueError:
-        print("\nOption ivalid!") 
-
-
+            print('Option unavailable.')
+    except ValueError:
+        print('Option unavailable.')
+menu()
--- product.py
+++ product.py
@@ -1,91 +1,79 @@
 class Product:
-    __id: int
+    __sku: int
     __name: str
-    __description: str
     __price: float
     __weight: float
+    __height: float
+    __length: float
     __width: float
-    __height: float    
-    __categories: list[str] = []      
-
+    __description: str
+    __categories: list
     
-    def get_id(self) -> int:
-        return self.__id
+    def get_categories(self) -> list:
+        return self.__categories
+    
+    def set_categories(self, category: str) -> None:
+        self.__categories.append(category)
 
-    def set_id(self, id: int) -> None:
-        self.__id = id
+    def set_sku(self, sku: str) -> None:
+        self.__sku = sku
 
+    def get_sku(self) -> str:
+        return self.__sku
 
-    def get_name(self) -> str:
-        return self.__name    
-        
     def set_name(self, name: str) -> None:
         self.__name = name
-    
+
+    def get_name(self) -> str:
+        return self.__name
+
+    def set_price(self, price: float) -> None:
+        self.__price = price
+
+    def get_price(self) -> float:
+        return self.__price
+
+    def set_weight(self, weight: float) -> None:
+        self.__weight = weight
+
+    def get_weight(self) -> float:
+        return self.__weight
+
+    def set_height(self, height: float) -> None:
+        self.__height = height
+
+    def get_height(self) -> float:
+        return self.__height
+
+    def set_length(self, length: float) -> None:
+        self.__length = length
+
+    def get_length(self) -> float:
+        return self.__length
+
+    def set_width(self, width: float) -> None:
+        self.__width = width
+
+    def get_width(self) -> float:
+        return self.__width
 
     def set_description(self, description: str) -> None:
         if len(description) >= 20:
             self.__description = description
         else:
-            print("Try again!")
+            print('Description must be bigger than 20 char')
 
     def get_description(self) -> str:
         return self.__description
 
-
-
-    def set_price(self, price: float) -> None:        
-        self.__price = price
-        
-
-    def get_price(self) -> float:
-        return self.__price
-
-
-    def set_weight(self, weight: float) -> None:
-        self.__weight = weight
-        
-
-    def get_weight(self) -> float:
-        return self.__weight
-
-    def set_width(self, width: float) -> None:
-        self.__width = width 
-      
-
-    def get_width(self) -> float:
-        return self.__width
-
-    def set_height(self, height: float) -> None:
-        self.__height = height
-    
-    def get_height(self) -> float:
-        return self.__height
-
-    def get_length(self) -> float:
-        return self.__length
-
-        def set_length(self, length: float) -> None:
-          self.__length = length
-
-    
-    
-    def get_categories(self) -> list:
-        return self.__categories
-
-    def append_categories(self, category: str) -> None:
-        self.__categories.append(category)
-
     def __str__(self):
-        return f"""
-                    - ID: {self.get_id()}
-                    - Name: {self.get_name()}
-                    - Price: {self.get_price()}
-                    - Weight: {self.get_weight()}
-                    - Height: {self.get_height()}
-                    - Length: {self.get_length()}
-                    - Width: {self.get_width()}
+        return f'''
+                    - SKU: {self.get_sku()} 
+                    - Name: {self.get_name()} 
+                    - Price: {self.get_price()} 
+                    - Weight: {self.get_weight()} 
+                    - Height: {self.get_height()} 
+                    - Length: {self.get_length()} 
+                    - Width: {self.get_width()} 
                     - Description: {self.get_description()}
-                 """
-
-  
+                '''
